ad817f43bf9a9d4199b3ed17dc14b1cf66d4091e
Samantha Lee <sammialicelee@yahoo.com>
2020-06-29 11:44:27 -0400

    converting html to ejs and adding app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+// Dependencies
+var express = require("express");
+
+// app initialization
+var app = express();
+
+// set view engine
+app.set("view engine", "ejs");
+
+var PORT = process.env.PORT || 3000;
